793188d339eb469e154b9e041c9a130a7e8c943e
ZDSeven <zedrickmigueldeguzman@gmail.com>
2023-12-08 12:02:22 +0800

    updated functions in Scanner class

--- Scanner.cpp
+++ Scanner.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
+/**
+ * Enumaration representing the different token types in the language. 
+ */
 enum class TokenType {
     O_Paren, C_Paren,
     Identifier,
@@ -10,33 +14,97 @@ enum class TokenType {
     Unknown
 };
 
+/**
+ * Class representing a token with its type and lexeme.
+ */
 class Token {
 public:
-    const TokenType type;
-    const std::string lexeme;
+    const TokenType type; /**< Type of the token. */
+    const std::string lexeme; /**< Lexeme of the token. */
 
-    Token(TokenType type, const std::string lexeme)
+    /**
+     * Constructor for Token class.
+     * @param type Type of the token.
+     * @param lexeme Lexeme of the token.
+     */
+    Token(TokenType type, const std::string& lexeme)
         : type(type), lexeme(lexeme) {};
 };
 
+/**
+ * Class responsible for scanning the source code and generating a sequence of tokens.
+ */
 class Scanner {
 private:
-    const std::string source;
-    std::vector<Token> tokens;
-    size_t current = 0;
-    size_t start = 0;
+    const std::string source; /**< Source code to be scanned. */
+    std::vector<Token> tokens; /**< Vector to store the generated tokens. */
+    size_t current = 0; /**< Current position in the source code. */
+    size_t start = 0; /**< Starting position of the current token being scanned. */
 
 public:
-    void scanToken(){}
+    /**
+     * Constructor for the Scanner class.
+     * @param source The source code to be scanned.
+     */
+    Scanner(const std::string& source) : source(source) {}
+    
+    /**
+     * Scans the next token from the source code and adds it to the list of tokens.
+     */
+    void scanToken() {
+        skipWhiteSpace();
+        /**
+         * Unordered map used to associate strings to their corresponding token types
+         */
+        char currentChar = source[current];
+        std::unordered_map<std::string, TokenType> tokenMap = {
+            {"(", TokenType::O_Paren},
+            {")", TokenType::C_Paren},
+            {"AND", TokenType::And},
+            {"NOT", TokenType::Not},
+            {"OR", TokenType::Or},
+            {"IMPLIES", TokenType::Implies},
+            {"EQUIVALENT", TokenType::Equivalent},
+        };
 
-    void scanIdentifier(){}
+        if (currentChar == '(') {
+            addToken(TokenType::O_Paren, std::string(1, currentChar));
+            current++;
+            return;
+        }
 
-    void nextToken() {}
+        std::string lexeme(1, currentChar);
+        while (std::isalpha(source[current + 1])) {
+            lexeme += source[++current];
+        }
 
-    void addToken(TokenType type, const std::string& lexeme){}
+        auto iterator = tokenMap.find(lexeme);
+        if (iterator != tokenMap.end()) {
+            addToken(iterator->second, lexeme);
+        } else if (lexeme == "P" || lexeme == "Q") {
+            addToken(TokenType::Identifier, lexeme);
+        } else {
+            addToken(TokenType::Unknown, lexeme);
+        }
 
-    void skipWhiteSpace(){}
+        current++;
+    }
 
+    /**
+     * Adds a token to the list of tokens with the given type and lexeme.
+     * @param type Type of the token to be added.
+     * @param lexeme Lexeme associated with the token.
+     */
+    void addToken(TokenType type, const std::string& lexeme) {
+        tokens.emplace_back(type, lexeme);
+    }
+
+    void skipWhiteSpace() {
+        while (std::isspace(source[current])) {
+            current++;
+        }
+    }
+    
     std::vector<Token> tokenize() {
         while (current < source.length()) {
             start = current;
@@ -44,8 +112,35 @@ public:
         }
 
         addToken(TokenType::TokenEOF, "");
-        
+
         return tokens;
     }
-
 };
+
+// Sample
+int main() {
+    std::string source = "(P OR Q) AND NOT Q";
+    Scanner scanner(source);
+
+    std::vector<Token> tokens = scanner.tokenize();
+
+    for (const Token& token : tokens) {
+        std::cout << "Type: ";
+        switch (token.type) {
+            case TokenType::O_Paren: std::cout << "O_Paren"; break;
+            case TokenType::C_Paren: std::cout << "C_Paren"; break;
+            case TokenType::Identifier: std::cout << "Identifier"; break;
+            case TokenType::Not: std::cout << "Not"; break;
+            case TokenType::And: std::cout << "And"; break;
+            case TokenType::Or: std::cout << "Or"; break;
+            case TokenType::Implies: std::cout << "Implies"; break;
+            case TokenType::Equivalent: std::cout << "Equivalent"; break;
+            case TokenType::TokenEOF: std::cout << "TokenEOF"; break;
+            case TokenType::Unknown: std::cout << "Unknown"; break;
+        }
+
+        std::cout << ", Lexeme: " << token.lexeme << std::endl;
+    }
+
+    return 0;
+}
