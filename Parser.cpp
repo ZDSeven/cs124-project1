c94cff1f4210983a0d98d82a22fc39043cf32a7e
J.Pean <astrofphinix@gmail.com>
2023-12-15 20:59:59 +0800

    added docstring comments

--- Parser.cpp
+++ Parser.cpp
@@ -3,21 +3,35 @@
 #include <string>
 #include "scanner.cpp"
 
+/**
+ * @brief Abstract base class representing a logical sentence
+ */
 class Sentence {
 public:
     virtual bool evaluate() const = 0;
 };
 
+/**
+ * @brief Concrete class representing a complex sentence with binary connective
+ */
 class ComplexSentence : public Sentence {
 private:
     Sentence* left;
     TokenType connective;
     Sentence* right;
-
+    /**
+     * @brief Constructor for ComplexSentence
+     * @param left Left operand
+     * @param connective Binary connective type
+     * @param right Right operand
+     */
 public:
     ComplexSentence(Sentence* left, TokenType connective, Sentence* right)
         : left(left), connective(connective), right(right) {}
-
+    /**
+     * @brief Evaluates the complex sentence based on the binary connective
+     * @return Result of the evaluation
+     */
     bool evaluate() const override {
         switch (connective) {
             case TokenType::And:
@@ -34,14 +48,24 @@ public:
         }
     }
 };
-
+/**
+ * @brief Concrete class representing an atomic sentence
+ */
 class AtomicSentence : public Sentence {
 private:
     std::string identifier;
 
 public:
+    /**
+     * @brief Constructor for AtomicSentence
+     * @param identifier Identifier for the atomic sentence
+     */
     AtomicSentence(const std::string& identifier) : identifier(identifier) {}
 
+    /**
+     * @brief Evaluates the atomic sentence (to be implemented)
+     * @return Result of the evaluation
+     */
     bool evaluate() const override {
         // Return identifier value
     }
@@ -50,23 +74,43 @@ public:
 class NegatedSentence : public Sentence {
 private:
     Sentence* innerSentence;
-
+/**
+ * @brief Concrete class representing a negated sentence
+ */
 public:
+/**
+     * @brief Constructor for NegatedSentence
+     * @param innerSentence Inner sentence to be negated
+     */
     NegatedSentence(Sentence* innerSentence) : innerSentence(innerSentence) {}
 
+    /**
+     * @brief Evaluates the negated sentence
+     * @return Result of the evaluation
+     */
     bool evaluate() const override {
         return !innerSentence->evaluate();
     }
 };
-
+/**
+ * @brief Parser class for parsing logical sentences
+ */
 class Parser {
 private:
     const std::vector<Token>& tokens;
     size_t current = 0;
 
 public:
+    /**
+     * @brief Constructor for Parser
+     * @param tokens Vector of tokens to be parsed
+     */
     Parser(const std::vector<Token>& tokens) : tokens(tokens) {}
-
+    /**
+     * @brief Helper function to match the current token with a given type
+     * @param type TokenType to match
+     * @return True if the match is successful, false otherwise
+     */
     bool match(TokenType type) {
         if (tokens[current].type == type) {
             current ++;
@@ -75,10 +119,18 @@ public:
         return false;
     }
 
+    /**
+     * @brief Parses a logical sentence
+     * @return Parsed sentence
+     */
     Sentence* parseSentence() {
         return parseComplex();
     }
 
+    /**
+     * @brief Parses a complex sentence with binary connective
+     * @return Parsed complex sentence
+     */
     Sentence* parseComplex() {
         Sentence* expr = parseEquivalent();
 
@@ -91,6 +143,10 @@ public:
         return expr;
     }
 
+    /**
+     * @brief Parses an equivalent sentence
+     * @return Parsed equivalent sentence
+     */
     Sentence* parseEquivalent() {
         Sentence* expr = parseImplies();
 
@@ -103,6 +159,10 @@ public:
         return expr;
     }
 
+    /**
+     * @brief Parses an implies sentence
+     * @return Parsed implies sentence
+     */
     Sentence* parseImplies() {
         Sentence* expr = parseOr();
 
@@ -115,6 +175,10 @@ public:
         return expr;
     }
 
+    /**
+     * @brief Parses an OR sentence
+     * @return Parsed OR sentence
+     */
     Sentence* parseOr() {
         Sentence* expr = parseAnd();
 
@@ -127,6 +191,10 @@ public:
         return expr;
     }
 
+    /**
+     * @brief Parses an AND sentence
+     * @return Parsed AND sentence
+     */
     Sentence* parseAnd() {
         Sentence* expr = parsePrimary();
 
@@ -138,7 +206,10 @@ public:
 
         return expr;
     }
-
+    /**
+     * @brief Parses a primary sentence
+     * @return Parsed primary sentence
+     */
     Sentence* parsePrimary() {
         if (match(TokenType::O_Paren)) {
             Sentence* innerSentence = parseComplex();
