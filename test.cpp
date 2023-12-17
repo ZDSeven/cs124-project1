cfa214fb17a46925704fd4b23b6e86c0b8b4b508
ZDSeven <zedrickmigueldeguzman@gmail.com>
2023-12-17 21:30:13 +0800

    updated

--- test.cpp
+++ test.cpp
@@ -1,82 +1,72 @@
-// #include <iostream>
-
-// // Base case for recursion (prints newline)
-// void printVars() {
-//     std::cout << std::endl;
-// }
-
-// // Recursive template function to print variables
-// template <typename T, typename... Args>
-// void printVars(T first, Args... args) {
-//     std::cout << first << ", ";
-//     printVars(args...);
-// }
-
-// int main() {
-//     int P = 10;
-//     double Q = 3.14;
-
-//     // Print one variable
-//     printVars(Q);
-
-//     // Print multiple variables
-//     printVars(P, Q);
-
-//     return 0;
-// }
 #include <iostream>
-#include <string>
-#include <map>
+#include <vector>
+#include "parser.cpp"
 
-bool evaluateExpression(const std::string& expression, const std::map<char, bool>& variableValues) {
-    std::string evaluatedExpression = expression;
+class Evaluator {
+public:
+    void setVariableValue(const std::string& name, bool value) {
+        AtomicSentence::setBooleanValue(name, value);
+    }
 
-    // Replace variable names with their corresponding values in the expression
-    for (const auto& pair : variableValues) {
-        char variable = pair.first;
-        bool value = pair.second;
+    bool getVariableValue(const std::string& name) const {
+        return AtomicSentence::getBooleanValue(name);
+    }
 
-        // Replace occurrences of the variable with its boolean value
-        size_t pos = 0;
-        while ((pos = evaluatedExpression.find(variable, pos)) != std::string::npos) {
-            evaluatedExpression.replace(pos, 1, value ? "true" : "false");
-            pos += value ? 4 : 5; // Length of "true" or "false"
+    bool evaluate(const Sentence* root) const {
+        return root->evaluate();
+    }
+};
+
+void printTruthTable(const std::vector<std::string>& variables, const Sentence* root) {
+    // Calculate the number of rows in the truth table
+    int numRows = 1 << variables.size(); // 2^n rows for n variables
+
+    // Print header
+    for (const std::string& variable : variables) {
+        std::cout << variable << "\t";
+    }
+    std::cout << "Result\n";
+
+    // Iterate through all possible truth values
+    for (int i = 0; i < numRows; ++i) {
+        Evaluator evaluator;
+
+        // Set truth values for variables
+        for (int j = 0; j < variables.size(); ++j) {
+            bool value = (i >> j) & 1; // Extract j-th bit
+            evaluator.setVariableValue(variables[j], value);
+            std::cout << std::boolalpha << value << "\t";
         }
-    }
 
-    // Evaluate the expression
-    std::string command = "echo \"(" + evaluatedExpression + ")\" | bc";
-    FILE* pipe = popen(command.c_str(), "r");
-    if (!pipe) {
-        std::cerr << "Error in evaluating the expression." << std::endl;
-        return false;
+        // Evaluate the logical expression for the current truth values
+        bool result = evaluator.evaluate(root);
+        std::cout << std::boolalpha << result << "\n";
     }
-
-    char buffer[128];
-    std::string result = "";
-    while (fgets(buffer, sizeof(buffer), pipe) != nullptr) {
-        result += buffer;
-    }
-
-    int exitCode = pclose(pipe);
-    return (exitCode == 0) && (result.find("1") != std::string::npos);
 }
 
 int main() {
-    std::map<char, bool> variableValues;
+    // Example logical expression: "P AND Q"
+    std::string sourceCode = "P AND Q";
 
-    // Input values for variables P, Q, and S
-    variableValues['P'] = true;  // Example value for P
-    variableValues['Q'] = false; // Example value for Q
-    variableValues['S'] = true;  // Example value for S
+    // Create a scanner and tokenize the source code
+    Scanner scanner(sourceCode);
+    std::vector<Token> tokens = scanner.tokenize();
 
-    std::string expression = "P && (!(Q && !S)) || Q";
+    // Create a parser and parse the logical expression
+    Parser parser(tokens);
+    Sentence* parsedSentence = parser.parseSentence();
 
-    // Evaluate the expression
-    bool result = evaluateExpression(expression, variableValues);
+    // Print the parse tree
+    std::cout << "\nParse Tree:\n";
+    printParseTree(parsedSentence, 0);
 
-    // Output the result
-    std::cout << "Result: " << (result ? "true" : "false") << std::endl;
+    // Print the truth table
+    std::cout << "\nTruth Table:\n";
+    std::vector<std::string> variables = {"P", "Q"}; // Add more variables as needed
+    printTruthTable(variables, parsedSentence);
+
+    // Clean up
+    delete parsedSentence;
 
     return 0;
 }
